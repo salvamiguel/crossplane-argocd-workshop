e91481d05796fe02141dee12500889d201df2f08
Salva Manzanera <salva.miguel.manzanera@gmail.com>
2024-11-19 20:15:49 +0100

    Actualiza la información de copyright y mejora los mensajes en el script de creación de credenciales de AWS; elimina el script de configuración del proveedor de AWS.

--- scripts/03-create-aws-credentials-secret.sh
+++ scripts/03-create-aws-credentials-secret.sh
@@ -2,8 +2,12 @@
 
 # Create AWS credentials file
 if [ ! -f "aws-credentials.txt" ]; then
+    echo "Creating AWS credentials file"
     touch aws-credentials.txt
     printf "[default]\naws_access_key_id = <PASTE HERE YOUR ACCESS KEY ID>\naws_secret_access_key = <PASTE HERE YOU SECRET ACCESS KEY>\n" > ./aws-credentials.txt
+    echo "Fill AWS credentials and re-run the script"
 else
+    kubectl create namespace crossplane-system
+    echo "AWS credentials file already exists"
     kubectl create secret generic aws-secret -n crossplane-system --from-file=aws-credentials.txt
 fi
