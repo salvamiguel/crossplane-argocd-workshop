b8ec8e93f9307788a00e8b84f394bbd63ec81c8b
Salva Manzanera <salva@viviendea.com>
2024-11-19 20:01:23 +0100

    Actualiza la información de copyright y mejora los mensajes en el script de creación de credenciales de AWS; elimina el script de configuración del proveedor de AWS.

--- scripts/configure-aws-provider.sh
+++
@@ -1,5 +0,0 @@
-#Create ProviderConfig
-kubectl apply -f ../aws-providers/provider-conf.yaml
-# Install Crossplane AWS providers
-kubectl apply -f aws-providers/ec2.yaml
-kubectl apply -f aws-providers/s3.yaml
