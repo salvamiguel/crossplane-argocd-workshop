b299d9c99ff2e2d43a8495f4d7f50051227d0acc
Salva Manzanera <salva.miguel.manzanera@gmail.com>
2024-11-27 16:48:44 +0100

    Fix typo

--- scripts/04-install-crossplane-chart.sh
+++ scripts/04-install-crossplane-chart.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Apply the Crossplane Helm secret configuration
-kubectl apply -f crossplane/crossplane-helm-secret.yaml
 # Apply the Crossplane custom bootstrap resource
 kubectl apply -f crossplane/crossplane-bootstrap.yaml
